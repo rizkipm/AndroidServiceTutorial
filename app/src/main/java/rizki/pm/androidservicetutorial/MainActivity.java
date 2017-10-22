a0666962543e4c7751cb806efcf00eba7ddde44f
Programmer Muslim <rizki@imastudio.co.id>
2017-10-22 21:06:47 +0700

    Initial commit

---
+++ app/src/main/java/rizki/pm/androidservicetutorial/MainActivity.java
@@ -0,0 +1,33 @@
+package rizki.pm.androidservicetutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnStart, btnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnStart = (Button)findViewById(R.id.buttonStart);
+        btnStop = (Button)findViewById(R.id.buttonStop);
+
+        btnStart.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == btnStart){
+
+        }else  if (v == btnStop){
+            
+        }
+
+    }
+}
